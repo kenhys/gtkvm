ec1422ae9a01c2a3c042c8db3922635e003ac5ad
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-05 01:29:18 +0900

    Setup jhbuild command

--- lib/gtkvm/cli.rb
+++ lib/gtkvm/cli.rb
@@ -24,6 +24,20 @@ module Gtkvm
 
     desc "bootstrap", "Bootstrap jhbuild."
     def bootstrap
+      gtkvm_path = File.expand_path("~/.gtkvm")
+      Dir.mkdir(gtkvm_path) unless File.exist?(gtkvm_path)
+      Dir.chdir(gtkvm_path) do
+        jhbuild_path = "#{gtkvm_path}/jhbuild"
+        FileUtils.rm_rf(jhbuild_path, :secure=>true)
+        `git clone git://git.gnome.org/jhbuild ~/.gtkvm/jhbuild`
+        Dir.chdir(jhbuild_path) do
+          Dir.mkdir("m4")
+          Dir.mkdir("build-aux")
+          `./autogen.sh`
+          `./configure --prefix=#{gtkvm_path}`
+          `make install`
+        end
+      end
     end
   end
 end
