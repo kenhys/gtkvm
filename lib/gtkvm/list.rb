ded248f5ec4449b82bc5a10d11115e0149b24fb1
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-13 01:33:20 +0900

    Add file for list subcomand

---
+++ lib/gtkvm/list.rb
@@ -0,0 +1,15 @@
+require 'thor'
+
+module Gtkvm
+
+  class List < Thor
+
+    desc "gtkvm", "List set of gtkvm"
+    def gtkvm
+    end
+
+    desc "moduleset", "List supported moduleset of gtkvm"
+    def moduleset
+    end
+  end
+end
