23469319f6ac16bc333ccafa021e44b8e7844606
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-19 01:27:48 +0900

    Make subdirectory under .gtkvm

--- lib/gtkvm/cli.rb
+++ lib/gtkvm/cli.rb
@@ -30,7 +30,10 @@ module Gtkvm
     desc "bootstrap", "Bootstrap jhbuild."
     def bootstrap
       gtkvm_path = File.expand_path("~/.gtkvm")
-      Dir.mkdir(gtkvm_path) unless File.exist?(gtkvm_path)
+      ["moduleset", "patches", "gtkset"].each do |directory|
+        path = "#{gtkvm_path}/jhbuildrc"
+        Dir.mkdir(path) unless File.exist?(path)
+      end
       Dir.chdir(gtkvm_path) do
         jhbuild_path = "#{gtkvm_path}/jhbuild"
         FileUtils.rm_rf(jhbuild_path, :secure=>true)
