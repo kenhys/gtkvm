bc167f7a0022f3e9a0218e9b1b375fa75bb593b6
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-19 02:21:57 +0900

    Extract code to detect configuration directory

--- lib/gtkvm/helper.rb
+++ lib/gtkvm/helper.rb
@@ -1,13 +1,18 @@
 module Gtkvm
   module Helper
 
-    def get_config_path
-      config_path = `pwd`.chomp + "/#{CONFIG_FILE}"
+    def get_config_dir
+      current_dir = `pwd`.chomp
+      config_path = File.join(current_dir, CONFIG_FILE)
       if File.exist?(config_path)
-        config_path
+        current_dir
       else
         File.expand_path("~/#{CONFIG_DIR}/#{CONFIG_FILE}")
       end
+    end
+
+    def get_config_path
+      File.expand_path(get_config_dir, CONFIG_FILE)
     end
 
     def get_config_file
