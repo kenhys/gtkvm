428931600fb608ba2da65f6bccec1b9b3d85f834
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-19 03:15:57 +0900

    Add clean command

--- lib/gtkvm/cli.rb
+++ lib/gtkvm/cli.rb
@@ -21,6 +21,32 @@ module Gtkvm
     def use(vmset)
     end
 
+    desc "clean", "Cleaning up gtkvmset"
+    def clean
+      config = get_config_file
+      vmset_dirs = Dir.glob("#{config['gtkvmset_dir']}/*").collect do |directory|
+        File.basename(directory)
+      end
+      gtkvmset = config['gtkvmset']
+      if vmset_dirs.empty?
+        config['gtkvmset'].each do |vmset|
+          puts "Cleaning up #{vmset}"
+        end
+        config['gtkvmset'] = []
+      else
+        vmset_dirs.each do |directory|
+          gtkvmset.delete_if do |vmset|
+            puts "Cleaning up #{vmset}" unless vmset_dirs.include?(vmset)
+            not vmset_dirs.include?(vmset)
+          end
+        end
+        config['gtkvmset'] = gtkvmset
+      end
+      File.open(get_config_path, "w+") do |file|
+        file.puts(YAML.dump(config))
+      end
+    end
+
     desc "create GTKVMSET", "Create recipe of gtkvmset."
     option :template
     def create(vmset)
