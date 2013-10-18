438a24a672d91a41c589af5654ebebdb07c826db
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-19 02:44:18 +0900

    Implement list gtkvm subcommand

--- lib/gtkvm/list.rb
+++ lib/gtkvm/list.rb
@@ -4,8 +4,19 @@ module Gtkvm
 
   class List < Thor
 
+    include Gtkvm::Helper
+
     desc "gtkvm", "List set of gtkvm"
     def gtkvm
+      config = get_config_file
+      gtkvmset_dir = config['gtkvmset_dir']
+      if config['gtkvmset'].nil?
+        puts "no gtkvmset in #{gtkvmset_dir}"
+      else
+        config['gtkvmset'].each do |gtkvmset|
+          puts "#{gtkvmset} [#{gtkvmset_dir}/#{gtkvmset}]"
+        end
+      end
     end
 
     desc "moduleset", "List supported moduleset of gtkvm"
