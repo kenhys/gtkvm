5d5359fabb929cae924f2f66c9f430cc72292b2d
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-20 01:43:52 +0900

    Implement build command

--- lib/gtkvm/build.rb
+++ lib/gtkvm/build.rb
@@ -3,6 +3,18 @@ module Gtkvm
 
     desc "build GTKVMSET", "Build specific gtkvmset."
     def build(vmset)
+      config = get_config_file
+      gtkvmset_dir = get_gtkvmset_dir(vmset)
+      tarball_dir = config['tarball_dir']
+      jhbuildrc_path = File.join(gtkvmset_dir, "jhbuildrc")
+      jhbuild_path = get_jhbuild_path
+      if File.exist?(jhbuildrc_path)
+        command="#{jhbuild_path} -f #{jhbuildrc_path}"
+        `#{command}`
+      else
+        message = "jhbuildrc doesn't exist for #{vmset.green}"
+        printf "%s: %s\n", "Error".red, message
+      end
     end
   end
 end
