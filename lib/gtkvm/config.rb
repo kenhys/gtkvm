f98b7fc363d94c6bdc5f35346282b3968fbd33c6
HAYASHI Kentaro <kenhys@gmail.com>
2013-10-19 17:44:44 +0900

    Implement config show subcommand

--- lib/gtkvm/config.rb
+++ lib/gtkvm/config.rb
@@ -1,11 +1,19 @@
 require 'thor'
+require 'pp'
 
 module Gtkvm
 
   class Config < Thor
 
+    include Gtkvm::Helper
+
     desc "show", "Show current configuration"
     def show
+      config = get_config_file
+      puts "gtkvm config:"
+      config.keys.each do |key|
+        printf " %24s: %s\n", key.green, config[key].to_s
+      end
     end
   end
 end
